fa89284fd28c6f31526f0ef12cd20e9c71b40762
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-22 12:46:37 +0530

    Update IAM.tf

--- Main_Ec2/IAM.tf
+++ Main_Ec2/IAM.tf
@@ -12,3 +12,13 @@ resource "aws_iam_role" "ec2_full_access" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "ec2_admin_access" {
+  role       = aws_iam_role.ec2_full_access.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_full_access_profile"
+  role = aws_iam_role.ec2_full_access.name
+}
